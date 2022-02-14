a73657ac61dd784527c61da9c3d89ef9611a8d62
MarcSicotte <sicotte13@live.fr>
2022-02-14 13:01:13 -0500

    fin_ex1

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <footer class="site__footer">
-    <ul class="footer__infos">
+    <ul class="site__footer__infos">
         <li>3800, rue Sherbrooke Est Montréal</li>
         <li>(Québec) H1X 2A2</li>
         <li> 514 254-7131</li>
         <li>blablablabla</li>
     </ul>
 
-    <nav class="footer__nav">
+    <nav class="site__footer__nav">
         <?php wp_nav_menu(array("menu"=>"footer",
                                 "container"=>"nav"));?>
     </nav>
 
-    <p class="footer__texte">
+    <p class="site__footer__texte">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod temporincididunt ut <br>
      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <br>
      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in <br>
