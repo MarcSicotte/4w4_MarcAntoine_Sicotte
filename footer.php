a50beb41167ed4c3f06376e8d710c0ddbcfef20b
MarcSicotte <sicotte13@live.fr>
2022-03-10 11:11:08 -0500

    widget

--- footer.php
+++ footer.php
@@ -1,31 +1,68 @@
 
 <footer class="site__footer">
-    <div class="site__footer__zone1">
+<!--<div class="site__footer_colonne">
+    <section class="footer__adresse">3800, rue Sherbrooke Est Montréal</section>
+    <section class="footer__article"></section>
+    <section class="footer__lien">
+        <?php
+            wp_nav_menu(array("menu"=>"lien_externe"));
+        ?>
+    </section>
+    
+<div class="site__footer_ligne">
+    <section class="footer__description">4w4-Conception d'interfaces et développement Web</section>
+    <section class="footer__copyright">&copy; Collège de Maisonneuve - Tout droit réserver</section>
+    <section class="footer__menu">
+        <?php /*
+             $icone = '<svg width="12px" height="12px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+            wp_nav_menu(array("menu"=>"footer",
+                                    "container"=>"nav",
+                                    "container_class" =>"footer__menu__nav",
+                                    "menu_class"=>"site__footer__menu__nav__ul",
+                                    "link_after"=> $icone));
+                                    
+        */?>
+    </section>
+    <section class="footer__recherche"> <?php /* get_search_form(); */?></section>
+    <section class="footer__Auteur">Marc-Antoine Sicotte</section>
+    
+</div> -->
+
+ <div class="site__footer__zone1">
     <ul class="infos">
         <li>3800, rue Sherbrooke Est Montréal</li>
         <li>(Québec) H1X 2A2</li>
         <li> 514 254-7131</li>
         <li>Intégration Multimédia</li>
     </ul>
+
+
+    <div class="menu3">
+        
+    </div>
     
+    
+
 
    
 
-    <h3 class="text">
-            Un site web fait par Marc-Antoine Sicotte 4w4 TIM
-    </h3>
-    </div>
+    <p class="text">
+        <?php get_sidebar( 'pied_page_zone2' ); ?>
+    </p>
+    </div> 
 
     <div class="site__footer__zone2">
     <?php 
              $icone = '<svg width="12px" height="12px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
-            wp_nav_menu(array("menu"=>"footer",
+            wp_nav_menu(array("menu"=>"secondaire",
                                     "container"=>"nav",
                                     "container_class" =>"site__footer__menu",
                                     "menu_class"=>"site__footer__menu__ul",
                                     "link_after"=> $icone));
                                     
-        ?>
+        ?> 
+        <section class="footer__recherche"> <?php  get_search_form(); ?></section>
+   
     </div>
     
 
--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
 function cidw_4w4_enregistre_mon_menu() {
   register_nav_menus( array(
     'principal'=> __( 'Menu Principal', 'cidw_4w4' ) ,
+    'lien_externe'=> __( 'Lien externe', 'cidw_4w4' ) ,
     'footer'=> __('Menu secondaire', 'cidw_4w4'))
   );
 }
@@ -41,5 +42,26 @@ add_filter("wp_nav_menu_objects", "cidw_4w4_filtre_le_menu");
 
 /******************************* les supports du thème *********************/
 add_theme_support('post-thumbnails');
+
+/******************************* Enregistrement des sidebar *********************/
+
+
+add_action( 'widgets_init', 'my_register_sidebars' );
+
+function my_register_sidebars() {
+  /* Register the 'primary' sidebar. */
+  register_sidebar(
+      array(
+          'id'            => 'pied_page_zone2',
+          'name'          => __( 'pied de page zone 2' ),
+          'description'   => __( 'A short description of the sidebar.' ),
+          'before_widget' => '<div id="%1$s" class="widget %2$s">',
+          'after_widget'  => '</div>',
+          'before_title'  => '<h3 class="widget-title">',
+          'after_title'   => '</h3>',
+      )
+  );
+
+}
 ?>
 
