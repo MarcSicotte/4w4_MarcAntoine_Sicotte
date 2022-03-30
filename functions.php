56dde400aefa2b9f5e9c52c3ba5bc8dcccf8ca2e
MarcSicotte <sicotte13@live.fr>
2022-03-30 17:47:28 -0400

    balises plus

--- functions.php
+++ functions.php
@@ -22,6 +22,7 @@ function cidw_4w4_enregistre_mon_menu() {
     'principal'=> __( 'Menu Principal', 'cidw_4w4' ) ,
     'lien_externe'=> __( 'Lien externe', 'cidw_4w4' ) ,
     'menu_categorie_cours'=> __( 'categorie cours', 'cidw_4w4' ) ,
+    'menu_accueil' =>__('Menu accueil', 'cidw_4w4'),
     'footer'=> __('Menu secondaire', 'cidw_4w4'))
   );
 }
